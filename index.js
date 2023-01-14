3450ecb2b22f0559b0609105a65e6e18051712c6
Analise <analise.giobbi@gc.com>
2023-01-14 12:33:13 -0500

    starting code for readme generator

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const callGenerateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your project title: ',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a project description: ',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation intstructions: ',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Describe how this project is used: ',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution instructions:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide testing instructions: ',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select your license: ',
+        choices: ['No License', 'MIT License', 'GNU Lesser General Public License v3.0', 'Mozilla Public License 2.0', 'GNU Affero General Public License v3.0', 'The Unlicense', 'Apache License 2.0', 'GNU General Public License v3.0'],
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your Github username: ',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email: ',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
